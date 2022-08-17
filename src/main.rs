80935584dafff6d23373208d81da7ec4852dd535
Tim Reber <tim.reber@bluewin.ch>
2022-08-17 10:14:48 +0200

    Working Project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    let arr = [1,2,3,4];
-    let hallo = "Tim";
-    print!("The Array {} is {} ", arr.len(), hallo)
+    //let options = ["Schere", "Stein", "Papier"];
+    let mut user_options = String::new();
+
+    println!("Select Operation: (1) Schere (2) Stein (3) Papier: ");
+    io::stdin()
+        .read_line(&mut user_options)
+        .expect("failed user input"); //&mut (Mutation) kann nicht geändert werden | io Modul (Input / Output)
+
+    let mut rng = rand::thread_rng();
+    let random = rng.gen_range(1..3);
+    println!("Computer: {}", random);
+
+    let user = match user_options.trim().parse() {
+        Ok(i) => i,
+        Err(_e) => -1,
+    };
+
+    println!("User: {}", user);
+
+    if random == user {
+        println!("Sie haben ein unentschieden gespielt!")
+    } else if user == 1 && random == 2 {
+        println!("Sie haben leider verloren!")
+    } else if user == 2 && random == 3 {
+        println!("Sie haben leider verloren!")
+    } else if user == 3 && random == 1 {
+        println!("Sie haben leider verloren!")
+    } else {
+        println!("Gratuliere, sie haben gewonnen!")
+    }
 }
